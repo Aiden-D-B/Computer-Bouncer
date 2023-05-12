76197c9bc7ed4a7c90ba4571f4dc166b926ce524
Aiden B <133374942+VeganMessiah@users.noreply.github.com>
2023-05-12 15:56:06 +0100

    Add files via upload

---
+++ Computer-Bouncer.py
@@ -0,0 +1,15 @@
+name = input("What is your name?:")
+print(name)
+if name =="Bob":
+    print("Welcome Bob!")
+else:
+    print("Sorry",name,"your not on the list")
+    print("You can pay £10 to join the club.")
+    price = input("Would you like to? (Yes/No)")
+if price =="Yes":
+    print("Ok, you are now on the list! Come on in!")
+else:
+    print("Ok then, good bye!")
+    
+    
+
